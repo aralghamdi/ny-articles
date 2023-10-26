0bdcafc6818b5aeb7b364acceacf8463ffbf2349
ar-alghamdi <aralghamdi96@gmail.com>
2023-10-26 03:41:35 +0300

    feat: initiate Dio with di

---
+++ lib/core/di.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get_it/get_it.dart';
+import 'package:ny_articles/core/data/remote/dio_client.dart';
+import 'package:ny_articles/utils/constants.dart';
+
+final di = GetIt.instance;
+
+Future<void> initDI() async {
+  final dio = initDio(dotenv.get(baseurl));
+  di.registerSingleton<Dio>(dio);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:ny_articles/core/di.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
+  await initDI();
   runApp(const MyApp());
 }
 
